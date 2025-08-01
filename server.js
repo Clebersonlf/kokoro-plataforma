2b6f614cbdcb003e19514bd8d17b62afb2bf0002
Cleberson Luiz <clebersonlf@gmail.com>
2025-08-01 01:24:27 -0300

    corrigido server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-> Cleberson:
-git add .
-git commit -m "Corrigido JSON e pronto para deploy"
-git push origin main
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
